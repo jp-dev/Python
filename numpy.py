792aba831fc9a9a8e0c76446634e31ae2bcab254
jp-dev <nixsterjp@gmail.com>
2018-12-23 21:53:38 +0900

    Update numpy.py
    Simple test file for numpy examples.

--- numpy.py
+++ numpy.py
@@ -1,7 +1,37 @@
+''' Test file
+'''
+
 import numpy as np
 
 a = np.array([1, 2, 3, 4])
 a + 1
 
 2**a
+
+
+
+b = np.ones(4) + 1
+a - b
+
+a * b
+
+
+j = np.arange(5)
+2**(j + 1) - j
+
+
+a = np.arange(10000)
+%timeit a + 1  
+
+l = range(10000)
+%timeit [i+1 for i in l] 
+
+
+c = np.ones((3, 3))
+c * c                   # NOT matrix multiplication!
+
+
+
+c.dot(c)
+
 
